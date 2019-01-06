8a8c37e04f8db64946f9e073a79c1d11f53596ac
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-06 15:22:25 +0100

    Updates

---
+++ precache-manifest.5e32b8f8d5b17528736f8267c144cd32.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c5a3701af199f1611a5",
+    "url": "/rtsk-async-example/static/js/runtime~main.8c5a3701.js"
+  },
+  {
+    "revision": "568a2a7d3444cc928271",
+    "url": "/rtsk-async-example/static/js/main.568a2a7d.chunk.js"
+  },
+  {
+    "revision": "25214771621ca0167afb",
+    "url": "/rtsk-async-example/static/js/1.25214771.chunk.js"
+  },
+  {
+    "revision": "e523dd6f9210df3e63c5c0efa6b948bb",
+    "url": "/rtsk-async-example/index.html"
+  }
+];
